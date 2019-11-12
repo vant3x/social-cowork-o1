217b13d10d9eb6f69d6deaed362ced29ca25a043
caborryer <corraleshyulia@gmail.com>
2019-11-12 18:24:44 -0500

    modificaciones

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,88 +1,139 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import './css/sb-admin-2.css';
-
-
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import "./css/sb-admin-2.css";
+import Swal from "sweetalert2";
+import usuarioAxios from "../config/axiosConfig";
 
 function Login() {
-  const handleClick = (e) => {
+  const [credenciales, guardarCredenciales] = useState({});
+
+  //Iniciar sesion en el servidor
+  const loguear = async e => {
     e.preventDefault();
-    localStorage.setItem('logged', true)
-    document.getElementById('redirect').click()
-  }
-  //const activarLoginFake = localStorage.setItem()
+
+    //autenticar el usuario
+    try {
+      const respuesta = await usuarioAxios.post("/login", credenciales);
+      console.log(respuesta.data);
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        type: "Error",
+        title: "Error, Usuario no registrado",
+        text: error.response.data.mensaje
+      });
+    }
+  };
+
+  const leerDatos = e => {
+    guardarCredenciales({ ...credenciales, [e.target.name]: e.target.value });
+  };
 
   return (
     <Fragment>
-
       <div>
-
         <div className="container">
-
-
-          <div className="row justify-content-center animated fadeIn">
-
-            <div className="col-xl-10 col-lg-12 col-md-9">
-
-              <div className="card o-hidden border-0 shadow-lg my-5">
-                <div className="card-body p-0">
-
-
-                  <div className="row">
-                    <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                    <div className="col-lg-6">
-                      <div className="p-5">
-                        <div className="text-center">
-                          <h1 className="h4 text-gray-900 mb-4">Bienvenido a Social CoWork</h1>
-                        </div>
-                        <form className="user">
-                          <div className="form-group">
-                            <input type="email" className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Email" />
+          <form onSubmit={loguear}>
+            <div className="row justify-content-center animated fadeIn">
+              <div className="col-xl-10 col-lg-12 col-md-9">
+                <div className="card o-hidden border-0 shadow-lg my-5">
+                  <div className="card-body p-0">
+                    <div className="row">
+                      <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                      <div className="col-lg-6">
+                        <div className="p-5">
+                          <div className="text-center">
+                            <h1 className="h4 text-gray-900 mb-4">
+                              Bienvenido a Social CoWork
+                            </h1>
                           </div>
-                          <div className="form-group">
-                            <input type="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
-                          </div>
-                          <div className="form-group">
-                            <div className="custom-control custom-checkbox small">
-                              <input type="checkbox" className="custom-control-input" id="customCheck" />
-                              <label className="custom-control-label" htmlFor="customCheck">Recuerdame</label>
+                          <form className="user">
+                            <div className="form-group">
+                              <input
+                                type="email"
+                                required
+                                className="form-control form-control-user"
+                                id="exampleInputEmail"
+                                aria-describedby="emailHelp"
+                                placeholder="Email"
+                                onChange={leerDatos}
+                              />
                             </div>
-                          </div>
-                          <button onClick={handleClick} className="btn btn-primary btn-user btn-block">
-                            Login
-                  </button>
-                          <Link to="/" id="redirect" >
-                          </Link>
+                            <div className="form-group">
+                              <input
+                                type="password"
+                                required
+                                className="form-control form-control-user"
+                                id="exampleInputPassword"
+                                placeholder="Password"
+                                onChange={leerDatos}
+                              />
+                            </div>
+                            <div className="form-group">
+                              <div className="custom-control custom-checkbox small">
+                                <input
+                                  type="checkbox"
+                                  className="custom-control-input"
+                                  id="customCheck"
+                                />
+                                <label
+                                  className="custom-control-label"
+                                  htmlFor="customCheck"
+                                >
+                                  Recuerdame
+                                </label>
+                              </div>
+                            </div>
+                            <button
+                              type="submit"
+                              value="iniciar sesion"
+                              className="btn btn-primary btn-user btn-block"
+                            >
+                              Login
+                            </button>
+                            <Link to="/" id="redirect"></Link>
+                            <hr />
+                            <a
+                              href="index.html"
+                              className="btn btn-google btn-user btn-block"
+                            >
+                              <i className="fab fa-google fa-fw"></i> Login con
+                              Google
+                            </a>
+                            <a
+                              href="index.html"
+                              className="btn btn-facebook btn-user btn-block"
+                            >
+                              <i className="fab fa-facebook-f fa-fw"></i> Login
+                              con Facebook
+                            </a>
+                          </form>
                           <hr />
-                          <a href="index.html" className="btn btn-google btn-user btn-block">
-                            <i className="fab fa-google fa-fw"></i> Login con Google
-                  </a>
-                          <a href="index.html" className="btn btn-facebook btn-user btn-block">
-                            <i className="fab fa-facebook-f fa-fw"></i> Login con Facebook
-                  </a>
-                        </form>
-                        <hr />
-                        <div className="text-center">
-                          <a className="small" href="/Recuperar">Olvide Mi Contraseña</a>
-                        </div>
-                        <div className="text-center">
-                          <Link to={"/Registro"} className="small" href="/Registro2">Registrarme</Link>
+                          <div className="text-center">
+                            <a className="small" href="/Recuperar">
+                              Olvide Mi Contraseña
+                            </a>
+                          </div>
+                          <div className="text-center">
+                            <Link
+                              to={"/Registro"}
+                              className="small"
+                              href="/Registro2"
+                            >
+                              Registrarme
+                            </Link>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-
             </div>
-
-          </div>
-
+          </form>
         </div>
       </div>
     </Fragment>
-  )
-
-
+  );
 }
 export default Login;
--- frontend/src/components/Registro.js
+++ frontend/src/components/Registro.js
@@ -1,150 +1,159 @@
-import React,{Fragment, useState, useEffect} from 'react';
-import './css/sb-admin-2.css';
-import fetchAxios from '../config/axiosConfig';
-import { withRouter } from 'react-router-dom';
+import React, { Fragment, useState, useEffect } from "react";
+import "./css/sb-admin-2.css";
+import Swal from "sweetalert2";
+import fetchAxios from "../config/axiosConfig";
+import { withRouter } from "react-router-dom";
 
+function Registro({ history }) {
+  const [usuario, guardarUsuario] = useState({
+    email: "",
+    password: "",
+    roleId: ""
+  });
 
-function Registro({history}){
+  // leer los datos del formulario
+  const actualizarState = e => {
+    // almacenar lo que el usuario escribe en el state
+    guardarUsuario({
+      // copia estado actual
+      ...usuario,
+      [e.target.name]: e.target.value
+    });
+    console.log(usuario);
+  };
 
-const [usuario, guardarUsuario] = useState({
-  email: '',
-  password: '',
-  roleId: ''
-});
+  // añade un nuevo usuario en la REST API
+  const agregarUsuario = e => {
+    e.preventDefault();
 
+    fetchAxios.post("/signup", usuario).then(res => {
+      Swal.fire({
+        type: "error",
+        tittle: "hubo un error ",
+        text: "El cliente ya esta registrado "
+      }).catch(() => {
+        console.log(res);
+        Swal.fire(
+          "se ha registrado exitosamente",
+          `${res.data.mensaje}`,
+          "success"
+        );
+      }); // redireccionar
+      history.push("/");
+    });
+  };
 
-// leer los datos del formulario
-    const actualizarState = e => {
-      // almacenar lo que el usuario escribe en el state
-      guardarUsuario({
-          // copia estado actual
-          ...usuario,
-          [e.target.name] : e.target.value
-      });
-      console.log(usuario);
-  }    
+  const validarUsuario = () => {
+    const { email, password, roleId } = usuario;
 
-      // añade un nuevo usuario en la REST API
-      const agregarUsuario = (e) => {
-        e.preventDefault();   
+    // revisar que las propiedades del state tengan contenido
+    let valido = !email.length || !password || !roleId.length;
 
-        fetchAxios.post('/signup',  usuario)
-            .then(res => {
-                // validar si hay errores de mongo
-                if (res.data.code === 11000) {
-                    console.log('Error de duplicado de Mongo');
-            
-                } else {
-                    console.log(res); 
-                
-                }
-                // redireccionar
-                history.push('/');
+    return valido;
+  };
 
-            });
-    }
+  return (
+    <Fragment>
+      <div className="">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-xl-10 col-lg-12 col-md-9">
+              <div className="card o-hidden border-0 shadow-lg my-5">
+                <div className="card-body p-0">
+                  <div className="row">
+                    <div className="col-lg-6 d-none d-lg-block bg-register-image"></div>
+                    <div className="col-lg-6">
+                      <div className="p-5">
+                        <div className="text-center">
+                          <h1 className="h4 text-gray-900 mb-4">
+                            Unete a Social CoWork
+                          </h1>
+                        </div>
+                        <form className="user" onSubmit={agregarUsuario}>
+                          <div className="form-group">
+                            <input
+                              type="email"
+                              className="form-control form-control-user"
+                              id="email"
+                              name="email"
+                              aria-describedby="emailHelp"
+                              placeholder="Email"
+                              onChange={actualizarState}
+                            />
+                          </div>
+                          <div className="form-group">
+                            <input
+                              type="password"
+                              className="form-control form-control-user"
+                              name="password"
+                              id="exampleInputPassword"
+                              placeholder="Password"
+                              onChange={actualizarState}
+                            />
+                          </div>
+                          <div class="form-group">
+                            <label for="exampleFormControlSelect1">
+                              Tipo Usuario
+                            </label>
+                            <select
+                              class="form-control"
+                              name="roleId"
+                              onChange={actualizarState}
+                              id="exampleFormControlSelect1"
+                            >
+                              <option value="0">Solo Reservas</option>
+                              <option value="1">ONG - Arrendatario</option>
+                            </select>
+                          </div>
+                          <div className="form-group">
+                            <div className="custom-control custom-checkbox small">
+                              <input
+                                type="checkbox"
+                                className="custom-control-input"
+                                id="customCheck"
+                              />
+                              <label
+                                className="custom-control-label"
+                                htmlFor="customCheck"
+                              >
+                                Recuerdame
+                              </label>
+                            </div>
+                          </div>
+                          <input
+                            type="submit"
+                            className="btn btn-primary btn-user btn-block"
+                            value="Registrarme"
+                            disabled={validarUsuario()}
+                          />
 
-    const validarUsuario = () => {
-      const {email, password, roleId} = usuario;
-
-      // revisar que las propiedades del state tengan contenido 
-      let valido = !email.length || !password || !roleId.length;
-
-      return valido;
-  }
-
-    
-
-  return(
-      <Fragment>
-
-<div className="">
-
-<div className="container">
-
- 
-  <div className="row justify-content-center">
-
-    <div className="col-xl-10 col-lg-12 col-md-9">
-
-      <div className="card o-hidden border-0 shadow-lg my-5">
-        <div className="card-body p-0">
-       
-
-          <div className="row">
-            <div className="col-lg-6 d-none d-lg-block bg-register-image"></div>
-            <div className="col-lg-6">
-              <div className="p-5">
-                <div className="text-center">
-                  <h1 className="h4 text-gray-900 mb-4">Unete a Social CoWork</h1>
-                </div>
-                <form className="user"  onSubmit={agregarUsuario} >
-                  <div className="form-group">
-                    <input 
-                      type="email" className="form-control form-control-user"
-                       id="email" name="email" aria-describedby="emailHelp" placeholder="Email" 
-                       onChange={actualizarState}
-
-                      />
-                  </div>
-                  <div className="form-group">
-                    <input type="password" className="form-control form-control-user" 
-                      name="password" id="exampleInputPassword" 
-                      placeholder="Password"
-                      onChange={actualizarState}
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleFormControlSelect1">Tipo Usuario</label>
-                    <select class="form-control" name="roleId"   
-                    onChange={actualizarState}
-                            id="exampleFormControlSelect1">
-                      <option value="0">Solo Reservas</option>
-                      <option value="1">ONG - Arrendatario</option>
-
-                    </select>
-                  </div>
-                  <div className="form-group">
-                    <div className="custom-control custom-checkbox small">
-                      <input type="checkbox" className="custom-control-input" id="customCheck"/>
-                      <label className="custom-control-label" htmlFor="customCheck">Recuerdame</label>
+                          <hr />
+                          <a
+                            href="index.html"
+                            className="btn btn-google btn-user btn-block"
+                          >
+                            <i className="fab fa-google fa-fw"></i> Registrarme
+                            con Google
+                          </a>
+                          <a
+                            href="index.html"
+                            className="btn btn-facebook btn-user btn-block"
+                          >
+                            <i className="fab fa-facebook-f fa-fw"></i>{" "}
+                            Registrarme con Facebook
+                          </a>
+                        </form>
+                        <hr />
+                      </div>
                     </div>
                   </div>
-                  <input type="submit" className="btn btn-primary btn-user btn-block"
-                        value="Registrarme"
-                        disabled={validarUsuario ()}
-
-                  />
-
-                  
-                  <hr/>
-                  <a href="index.html" className="btn btn-google btn-user btn-block">
-                    <i className="fab fa-google fa-fw"></i> Registrarme con Google
-                  </a>
-                  <a href="index.html" className="btn btn-facebook btn-user btn-block">
-                    <i className="fab fa-facebook-f fa-fw"></i> Registrarme con Facebook
-                  </a>
-                </form>
-                <hr/>
-              
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-    </div>
-
-  </div>
-
-</div>
-</div>
-
-
-
-      </Fragment>
-  )
-
-
+    </Fragment>
+  );
 }
 export default withRouter(Registro);
